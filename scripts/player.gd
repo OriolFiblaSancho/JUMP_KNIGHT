dbf70920afcdbe7f11901c717af3d5d99e0a83c7
oriolfibla <oriolfibla@iesmontsia.org>
2025-02-04 18:54:34 +0100

    added raycasting to fix wall sliding and wall jumping upgraded test lvl and changed player sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+@onready var rayWall = $RayWall
 
-const SPEED = 400.0
+const SPEED = 600.0
 const JUMP_VELOCITY = -500.0
-const WALL_PUSHBACK_X = 1800
-const WALL_PUSHBACK_Y = -600
-const WALL_SLIDE_MAX = 50
+const WALL_PUSHBACK_X = 2000
+const WALL_PUSHBACK_Y = -400
+const WALL_SLIDE_MAX = 200
 
 var jumping = 0
 var debug_counter = 0
@@ -16,16 +17,11 @@ func _physics_process(delta: float) -> void:
 	
 	#DEBUGGING-----------------------------------------
 	print("----",debug_counter,"----")
-	print("VEL X - ",velocity.x)
-	print("VEL Y - ",velocity.y)
+	print("DIR - ",direction)
+	print("VEL - ",velocity)
 	print("JUMP - ",jumping)
-	print("WALL_X - ", wall_normal.x)
-	if is_on_wall_only():
-		print("WALL - TRUE")
-	else:
-		print("WALL - FALSE")
 	debug_counter += 1
-	#---------------------------------------------------
+	#---------------------------------------------------	
 	
 	#GRAVITY HANDLING
 	if not is_on_floor():
@@ -33,32 +29,36 @@ func _physics_process(delta: float) -> void:
 	else:
 		jumping = 0 #Reset jumping when touching ground
 	
-
-	#WALL SLIDING
-	if is_on_wall_only():
-		velocity.y = min(velocity.y, WALL_SLIDE_MAX)
-		
-		#IF IS ON WALL AND "JUMP":
-		if Input.is_action_just_pressed("ui_accept"):
-			# Push player away from the wall
-			velocity.x = wall_normal.x * WALL_PUSHBACK_X
-			velocity.y = WALL_PUSHBACK_Y
-			jumping = 1
-			
-	#JUMP HANDLING
-	elif Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jumping = 1
 	
-	#DOUBLE JUMP HANDLING
-	elif Input.is_action_just_pressed("ui_accept") and jumping == 1:
-		velocity.y = JUMP_VELOCITY
-		jumping = 0
-
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jumping = 1
+		elif wall_colider():
+			velocity = wall_normal * WALL_PUSHBACK_X
+			velocity.y = WALL_PUSHBACK_Y
+		elif jumping == 1:
+			velocity.y = JUMP_VELOCITY
+			jumping = 0
+	
+			
+	#WALL SLIDING
+	if wall_colider():
+		velocity.y = min(velocity.y, WALL_SLIDE_MAX)
+		jumping = 1
+		
 	#HANDLE X DIRECTION
 	if direction:
 		velocity.x = direction * SPEED
+		if direction == 1:
+			rayWall.scale.x = 1
+		elif direction == -1:
+			rayWall.scale.x = -1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
 	move_and_slide()
+
+func wall_colider():
+	return rayWall.is_colliding()
