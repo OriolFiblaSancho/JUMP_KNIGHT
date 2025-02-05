d2fad4e71b206910a95667e8d56bd32a6ff5d4cd
OriolFiblaSancho <oriolfibla@iesmontsia.org>
2025-02-05 23:29:08 +0100

    Added: Coyote time X and Y acceleration Smooth camera movement
    Fixed:
    Wall jump
    Made the script more readable

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,48 +1,47 @@
 extends CharacterBody2D
 
 @onready var rayWall = $RayWall
+@onready var coyote_time = $CoyoteTime
 
 const SPEED = 600.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -600.0
 const WALL_PUSHBACK_X = 700
 const WALL_PUSHBACK_Y = -700
 const WALL_SLIDE_MAX = 300
 const ACCELERATION_x = SPEED * 7 # increase to max speed in 1/7th of a second
-const ACCELERATION_y = 0.05
+const ACCELERATION_y = 20
+const MAX_ACCELERATION_y = 1200
 
 var jumping = 0 #IS JUMPING ?
+var fall_acceleration = 0.0 #stores fall acc
 var debug_counter = 0
 
-func _physics_process(delta: float) -> void:
-	
+func _physics_process(delta: float):
+
 	var direction := Input.get_axis("ui_left", "ui_right")
+	var was_on_floor = is_on_floor()
 	
-	#DEBUGGING-----------------------------------------
-	print("----",debug_counter,"----")
-	print("DIR - ",direction)
-	print("VEL - ",velocity)
-	print("JUMP - ",jumping)
-	print("GRAVITY - ", get_gravity())
-	debug_counter += 1
-	#---------------------------------------------------	
-	
-	#GRAVITY HANDLING
+	# GRAVITY HANDLING
 	if not is_on_floor():
-		velocity.y += move_toward(velocity.y, 2, ACCELERATION_y * delta)
+		#Calculates acceleration and add it to the var
+		fall_acceleration = min(fall_acceleration + ACCELERATION_y * delta * 100, MAX_ACCELERATION_y)
+		velocity.y += (500 + fall_acceleration) * delta
 	else:
+		fall_acceleration = 0  # Resetear aceleración al tocar el suelo
 		if jumping != 0:
-			jumping = 0 #Reset jumping when touching ground
+			jumping = 0  # Reset jumping when touching ground
 	
 	
 	#HANDLES JUMP THINGS
 	if Input.is_action_just_pressed("ui_accept"):
-		if is_on_floor():		#HANDLES JUMP
+		if is_on_floor() || !coyote_time.is_stopped():	#HANDLES JUMP and coyote time
 			velocity.y = JUMP_VELOCITY
 			jumping = 1
 		elif wall_colider():	#HANDLES WALL JUMP
 			wall_jump(get_wall_normal())
 		elif jumping == 1:		#HANDLES DOUBLE JUMP
-			double_jump()
+			pass
+			#double_jump()
 	
 			
 	#WALL SLIDING
@@ -61,6 +60,23 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
+	#Check if it was on floor after move_and_slide()
+	if was_on_floor && !is_on_floor():
+		coyote_time.start()
+		
+	#DEBUGGING-----------------------------------------
+	print("----",debug_counter,"----")
+	print("DIR - ",direction)
+	print("VEL - ",velocity)
+	print("JUMP - ",jumping)
+	print("WAS - ", was_on_floor)
+	print("IS - ",is_on_floor())
+	if coyote_time.is_stopped():
+		print("COYOTE - STOPED")
+	else:
+		print("COYOTE - RUNNING")
+	debug_counter += 1
+	#---------------------------------------------------	
 	
 #RAYCAST FUNC TO SEE IF IS COLLIDING WITH A WALL
 func wall_colider():
@@ -70,7 +86,7 @@ func wall_colider():
 func double_jump():
 	velocity.y = JUMP_VELOCITY
 	jumping = 0
-	
+	fall_acceleration = 0
 #WALL JUMP FUNC
 func wall_jump(wall_normal):
 	velocity.x = wall_normal.x * WALL_PUSHBACK_X
